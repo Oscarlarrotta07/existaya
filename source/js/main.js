27da815827b693db46d87a29c33dde82d1ef530d
Oscarlarrotta07 <oscarlarrotta@hotmail.com>
2021-09-04 18:00:49 -0500

    adding js of cards, and superheroesPugs

--- source/js/main.js
+++ source/js/main.js
@@ -1,81 +1,152 @@
-class Hero {
-  constructor() {
-    this.$el = document.querySelector(".hero");
-
-    if (this.$el) {
-      this.name = this.$el.getAttribute("data-name").toLowerCase();
-      this.$likeBtn = this.$el.querySelector(".hero__buttonLike");
-      this.$dislikeBtn = this.$el.querySelector(".hero__buttonDislike");
-      this.$likeBar = this.$el.querySelector(".bar__like");
-      this.$likeBarText = this.$likeBar.querySelector(".bar__number");
-      this.$dislikeBar = this.$el.querySelector(".bar__dislike");
-      this.$dislikeBarText = this.$dislikeBar.querySelector(".bar__number");
-      this.$voteEl = this.$el.querySelector(".hero__vote");
-
-      const persistedData = JSON.parse(localStorage.getItem(this.name)) || {};
-      this.likes = persistedData.likes || 0;
-      this.dislikes = persistedData.dislikes || 0;
-      this.votes = persistedData.votes || 0;
-      this.addEvents();
-      this.updateBar();
-      this.persistHero();
-    }
-  }
-
-  addEvents() {
-    const self = this;
-
-    if (this.$likeBtn) {
-      this.$likeBtn.addEventListener("click", function () {
-        self.likes += 1;
-        self.votes += 1;
-        self.updateBar();
-        self.$voteEl.classList.add("hero__vote-like");
-        self.persistHero();
-      });
-    }
-
-    if (this.$dislikeBtn) {
-      this.$dislikeBtn.addEventListener("click", function () {
-        self.dislikes += 1;
-        self.votes += 1;
-        self.updateBar();
-        self.$voteEl.classList.add("hero__vote-dislike");
-        self.persistHero();
-      });
-    }
-  }
-
-  persistHero() {
-    if (localStorage) {
-      localStorage.setItem(
-        this.name,
-        JSON.stringify({
-          likes: this.likes,
-          dislikes: this.dislikes,
-          votes: this.votes,
-        })
-      );
-    }
-  }
-
-  calculateVotes(total, votes) {
+(function () {
+  function calculateVotes(total, votes) {
     if (total === 0) return 50;
     if (votes === 0) return 0;
     return ((votes / total) * 100).toFixed(1);
   }
 
-  updateBar() {
-    const likesPorcentage = this.calculateVotes(this.votes, this.likes);
-    const dislikesPorcentage = this.calculateVotes(this.votes, this.dislikes);
-    const likesText = `${likesPorcentage}%`;
-    const dislikesText = `${dislikesPorcentage}%`;
-    this.$likeBar.style.width = likesText;
-    this.$likeBarText.innerHTML = likesText;
-
-    this.$dislikeBar.style.width = dislikesText;
-    this.$dislikeBarText.innerHTML = dislikesText;
+  function getPercetange(votes, likes, dislikes) {
+    const likesPercentage = calculateVotes(votes, likes);
+    const dislikesPercentage = calculateVotes(votes, dislikes);
+    return {
+      likesPercentage: `${likesPercentage}%`,
+      dislikesPercentage: `${dislikesPercentage}%`,
+    };
   }
-}
 
-const hero = new Hero();
+  class Hero {
+    constructor() {
+      console.log("hero");
+      this.$el = document.querySelector(".hero");
+
+      if (this.$el) {
+        this.name = this.$el.getAttribute("data-name");
+        this.$likeBtn = this.$el.querySelector(".hero__buttonLike");
+        this.$dislikeBtn = this.$el.querySelector(".hero__buttonDislike");
+        this.$likeBar = this.$el.querySelector(".bar__like");
+        this.$likeBarText = this.$likeBar.querySelector(".bar__number");
+        this.$dislikeBar = this.$el.querySelector(".bar__dislike");
+        this.$dislikeBarText = this.$dislikeBar.querySelector(".bar__number");
+        this.$voteEl = this.$el.querySelector(".hero__vote");
+
+        const persistedData = JSON.parse(localStorage.getItem(this.name)) || {};
+        this.likes = persistedData.likes || 0;
+        this.dislikes = persistedData.dislikes || 0;
+        this.votes = persistedData.votes || 0;
+        this.addEvents();
+        this.updateBar();
+        this.persistHero();
+      }
+    }
+
+    addEvents() {
+      const self = this;
+
+      if (this.$likeBtn) {
+        this.$likeBtn.addEventListener("click", function () {
+          self.likes += 1;
+          self.votes += 1;
+          self.updateBar();
+          self.$voteEl.classList.add("hero__vote-like");
+          self.persistHero();
+        });
+      }
+
+      if (this.$dislikeBtn) {
+        this.$dislikeBtn.addEventListener("click", function () {
+          self.dislikes += 1;
+          self.votes += 1;
+          self.updateBar();
+          self.$voteEl.classList.add("hero__vote-dislike");
+          self.persistHero();
+        });
+      }
+    }
+
+    persistHero() {
+      if (localStorage) {
+        localStorage.setItem(
+          this.name,
+          JSON.stringify({
+            likes: this.likes,
+            dislikes: this.dislikes,
+            votes: this.votes,
+          })
+        );
+      }
+    }
+
+    calculateVotes(total, votes) {
+      if (total === 0) return 50;
+      if (votes === 0) return 0;
+      return ((votes / total) * 100).toFixed(1);
+    }
+
+    updateBar() {
+      const likesPercentage = getPercetange(
+        this.votes,
+        this.likes,
+        this.dislikes
+      ).likesPercentage;
+
+      const dislikesPercentage = getPercetange(
+        this.votes,
+        this.likes,
+        this.dislikes
+      ).dislikesPercentage;
+
+      // const { likesPercentage, dislikesPercentage } = getPercetange(
+      //   this.votes,
+      //   this.likes,
+      //   this.dislikes
+      // );
+
+      this.$likeBar.style.width = likesPercentage;
+      this.$likeBarText.innerHTML = likesPercentage;
+      this.$dislikeBar.style.width = dislikesPercentage;
+      this.$dislikeBarText.innerHTML = dislikesPercentage;
+    }
+  }
+
+  class Card {
+    constructor($el) {
+      this.name = $el.getAttribute("data-name");
+      const persistedData = JSON.parse(localStorage.getItem(this.name)) || {};
+      this.likes = persistedData.likes || 0;
+      this.dislikes = persistedData.dislikes || 0;
+      this.votes = persistedData.votes || 0;
+      this.$likeBar = $el.querySelector(".bar__like");
+      this.$likeBarText = this.$likeBar.querySelector(".bar__number");
+      this.$dislikeBar = $el.querySelector(".bar__dislike");
+      this.$dislikeBarText = this.$dislikeBar.querySelector(".bar__number");
+      this.updateCard();
+    }
+
+    updateCard() {
+      const likesText = getPercetange(
+        this.votes,
+        this.likes,
+        this.dislikes
+      ).likesPercentage;
+      const dislikesText = getPercetange(
+        this.votes,
+        this.likes,
+        this.dislikes
+      ).dislikesPercentage;
+
+      this.$likeBar.style.width = likesText;
+      this.$likeBarText.innerHTML = likesText;
+
+      this.$dislikeBar.style.width = dislikesText;
+      this.$dislikeBarText.innerHTML = dislikesText;
+    }
+  }
+
+  new Hero();
+  const cards = document.querySelectorAll(".card");
+  if (cards) {
+    cards.forEach(function (card) {
+      new Card(card);
+    });
+  }
+})();
